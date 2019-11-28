538db2bdb3557b15e5e70b1ebbf558f24d21c08e
Xing Xudong <50110585+abexope@users.noreply.github.com>
2019-11-28 11:15:35 +0800

    添加Dev分支

--- demo.py
+++ demo.py
@@ -2,11 +2,8 @@
 test demo
 """
 
-from swarm.particle import Particle
-from swarm.particle import ParticleSwarm
+import numpy as np
 from optimizer.pso import ParticleSwarmOptimization as PSO
-from optimizer.pso import ParticleSwarmOptimization2 as PSO2
-from evaluator.base import FitnessFunction
 from evaluator.base import FitnessFunction2
 import matplotlib.pyplot as plt
 
@@ -14,40 +11,19 @@ import matplotlib.pyplot as plt
 def demo_pso1():
 	D = 2
 	pop_size = 50
-	max_iter = 100
-	pop_max = [10, 10]
-	pop_min = [-10, -10]
-	V_max = [1, 1]
-	V_min = [-1, -1]
+	max_iter = 100000
+	pop_max = np.array([10, 10])
+	pop_min = np.array([-10, -10])
+	V_max = np.array([1, 1])
+	V_min = np.array([-1, -1])
 
-	swarm = [Particle(D, pop_max, pop_min, V_max, V_min) for _ in range(pop_size)]
-	pso = PSO(evaluator=FitnessFunction, swarm=swarm, population_size=pop_size)
+	pso = PSO(D, pop_size, max_iter, pop_max, pop_min, V_max, V_min, evaluator=FitnessFunction2)
 	pso.search()
 
-	plt.figure()
-	plt.plot(range(max_iter), pso.yy)
+	plt.figure(figsize=(4, 3))
+	plt.loglog(range(max_iter), pso.yy)
 	plt.grid()
-	plt.ylim([0, 0.1])
-
-	# plt.show()
-
-
-def demo_pso2():
-	D = 2
-	pop_size = 50
-	max_iter = 100
-	pop_max = [10, 10]
-	pop_min = [-10, -10]
-	V_max = [1, 1]
-	V_min = [-1, -1]
-	swarm = ParticleSwarm(D, pop_size, pop_max, pop_min, V_max, V_min)
-	pso = PSO2(epoch=max_iter, evaluator=FitnessFunction2, swarm=swarm, population_size=pop_size)
-	pso.search()
-
-	plt.figure()
-	plt.plot(range(max_iter), pso.yy)
-	plt.grid()
-	plt.ylim([0, 0.1])
+	# plt.ylim([1e-10, 0.1])
 
 	plt.show()
 
@@ -58,11 +34,6 @@ if __name__ == '__main__':
 	# t1 = time.time()
 	# for _ in range(100):
 	# 	demo_pso1()
-	# print("面向个体：", time.time() - t1)
-	#
-	# t2 = time.time()
-	# for _ in range(100):
-	# 	demo_pso2()
-	# print("面向种群：", time.time() - t2)
+	# print((time.time() - t1) / 100)
+
 	demo_pso1()
-	demo_pso2()
--- swarm/particle.py
+++ swarm/particle.py
@@ -2,223 +2,11 @@
 Particle object module
 """
 
-from abc import abstractmethod, ABCMeta
 import numpy as np
 from numpy.random import uniform
 
 
-class BaseParticle(metaclass=ABCMeta):
-	"""
-	Basic particle template
-	"""
-	
-	@abstractmethod
-	def __init__(self, dimension, upper_bound, lower_bound, *args):
-		"""
-		Initialization method
-		:param dimension:
-			space dimension
-			python.int
-		:param upper_bound:
-			upper bound of searching space for each dimension
-			1-Axis array like, only python list, numpy.array and pandas.Series are supported.
-			Any data structure is always transformed to numpy.array
-		:param lower_bound:
-			upper bound of searching space for each dimension
-			1-Axis array like, only python list, numpy.array and pandas.Series are supported.
-			Any data structure is always transformed to numpy.array
-		:param args:
-			other inputs
-		"""
-		self.D = dimension
-		self.upper = upper_bound
-		self.lower = lower_bound
-		self.__transform_pos_type()
-	
-	def __transform_pos_type(self):
-		if not isinstance(self.upper, np.ndarray):
-			self.upper = np.array(self.upper)
-		if not isinstance(self.lower, np.ndarray):
-			self.lower = np.array(self.lower)
-		assert self.upper.ndim == self.lower.ndim == 1
-		assert len(self.upper) == len(self.lower) == self.D
-
-	@abstractmethod
-	def evolve(self, *args):
-
-		pass
-	
-	@abstractmethod
-	def correct(self, *args):
-
-		pass
-
-
-class BaseSwarm(metaclass=ABCMeta):
-	"""Basic swarm template"""
-
-	@abstractmethod
-	def __init__(
-			self, dimension, population,
-			# evaluator,
-			upper_bound, lower_bound, *args
-	):
-		self.D = dimension
-		self.population = population
-		# self.evaluator = evaluator
-		self.upper = upper_bound
-		self.lower = lower_bound
-		self.__transform_bound_type()
-		self.position = self.lower + (self.upper - self.lower) \
-			* uniform(0, 1, size=(self.population, self.D))     # position initialize
-
-	def __transform_bound_type(self):
-		if not isinstance(self.upper, np.ndarray):
-			self.upper = np.array(self.upper)
-		if not isinstance(self.lower, np.ndarray):
-			self.lower = np.array(self.lower)
-		assert self.upper.ndim == self.lower.ndim == 1
-		assert len(self.upper) == len(self.lower) == self.D
-
-	@abstractmethod
-	def evolve(self, *args):
-
-		pass
-
-
-class Particle(BaseParticle):
-	"""
-	Particle object, the individual of basic PSO algorithm.
-	"""
-	
-	def __init__(
-			self, dimension, upper_bound, lower_bound,
-			upper_velocity, lower_velocity,
-			acceleration_coeff1=2, acceleration_coeff2=2,
-	):
-		"""
-		Initialization method
-		:param dimension:
-			space dimension
-			python.int
-		:param upper_bound:
-			upper bound of searching space for each dimension
-			1-Axis array like, only python list, numpy.array and pandas.Series are supported.
-			Any data structure is always transformed to numpy.array
-		:param lower_bound:
-			lower bound of searching space for each dimension
-			1-Axis array like, only python list, numpy.array and pandas.Series are supported.
-			Any data structure is always transformed to numpy.array
-		:param upper_velocity:
-			upper velocity of the particle for each dimension
-			1-Axis array like, only python list, numpy.array and pandas.Series are supported.
-			Any data structure is always transformed to numpy.array
-		:param lower_velocity:
-			lower velocity of the particle for each dimension
-			1-Axis array like, only python list, numpy.array and pandas.Series are supported.
-			Any data structure is always transformed to numpy.array
-		:param acceleration_coeff1:
-			acceleration coefficient of individual history impact.
-			python.int
-		:param acceleration_coeff2:
-			acceleration coefficient of the best individual in the whole swarm.
-			python.int
-		"""
-		super(Particle, self).__init__(dimension, upper_bound, lower_bound)
-		self.V_max = upper_velocity
-		self.V_min = lower_velocity
-		self.__transform_vel_type()
-		self.c1 = acceleration_coeff1
-		self.c2 = acceleration_coeff2
-		self.position = self.lower + (self.upper - self.lower) * uniform(0, 1, size=(self.D,))
-		self.velocity = self.V_min + (self.V_max - self.V_min) * uniform(0, 1, size=(self.D,))
-		self.pbest = self.position      # the personal history best position of a particle
-		
-	def __transform_vel_type(self):
-		if not isinstance(self.V_max, np.ndarray):
-			self.V_max = np.array(self.V_max)
-		if not isinstance(self.V_min, np.ndarray):
-			self.V_min = np.array(self.V_min)
-		assert self.V_max.ndim == self.V_min.ndim == 1
-		assert len(self.V_max) == len(self.V_min) == self.D
-		
-	def evolve(self, gbest, *args):
-		"""
-		Evolution method of basic particle in PSO algorithm
-		:param gbest:
-			position of the best individual in the whole swarm
-			1-Axis numpy.array
-		:param args:
-			other params
-		:return:
-		"""
-		self.velocity = self.velocity \
-			+ self.c1 * uniform(0, 1) * (self.pbest - self.position) \
-			+ self.c2 * uniform(0, 1) * (gbest - self.position)     # update the velocity
-		self.position = self.position + self.velocity               # update the position
-	
-	def correct(self, *args):
-		"""
-		Correction method
-			after once iteration, the values that are beyond the thresholds should be corrected.
-		:param args:
-		:return:
-		"""
-		self.position = np.minimum(self.position, self.upper)
-		self.position = np.maximum(self.position, self.lower)
-		self.velocity = np.minimum(self.velocity, self.V_max)
-		self.velocity = np.maximum(self.velocity, self.V_min)
-
-
-class ParticleSwarm(BaseSwarm):
-
-	def __init__(
-			self, dimension, population,
-			# evaluator,
-			upper_bound, lower_bound,
-			upper_velocity, lower_velocity, acceleration_coeff1=2, acceleration_coeff2=2,
-			opt_mode="minimum",
-	):
-		super(ParticleSwarm, self).__init__(
-			dimension, population,
-			# evaluator,
-			upper_bound, lower_bound
-		)
-		assert opt_mode == "minimum" or "maximum"
-		self.c1 = acceleration_coeff1
-		self.c2 = acceleration_coeff2
-		self.V_max = upper_velocity
-		self.V_min = lower_velocity
-		self.__transform_vel_type()
-		self.velocity = self.V_min + (self.V_max - self.V_min) \
-			* uniform(0, 1, size=(self.population, self.D))
-		self.pbest = self.position      # initial personal best position
-
-	def __transform_vel_type(self):
-		if not isinstance(self.V_max, np.ndarray):
-			self.V_max = np.array(self.V_max)
-		if not isinstance(self.V_min, np.ndarray):
-			self.V_min = np.array(self.V_min)
-		assert self.V_max.ndim == self.V_min.ndim == 1
-		assert len(self.V_max) == len(self.V_min) == self.D
-
-	def evolve(self, gbest):
-		# update velocity
-		self.velocity = self.velocity \
-			+ self.c1 * uniform(0, 1, size=(self.population, self.D)) * (self.pbest - self.position) \
-			+ self.c2 * uniform(0, 1, size=(self.population, self.D)) * (gbest - self.position)
-		self.__correct()
-		self.position = self.position + self.velocity      # update position
-		self.__correct()        # correct position
-
-	def __correct(self):
-		self.position = np.minimum(self.position, self.upper)
-		self.position = np.maximum(self.position, self.lower)
-		self.velocity = np.minimum(self.velocity, self.V_max)
-		self.velocity = np.maximum(self.velocity, self.V_min)
-
-
-class ParticleSwarm2:
+class ParticleSwarm:
 	
 	def __init__(
 			self, dimension: int, population: int,
@@ -238,8 +26,7 @@ class ParticleSwarm2:
 		self.position = self.lower + (self.upper - self.lower) * uniform(0, 1, size=[self.population, self.D])
 		self.velocity = self.V_min + (self.V_max - self.V_min) * uniform(0, 1, size=[self.population, self.D])
 		
-		self.pbest = self.position
-		self.gbest = None
+		self.pbest = self.gbest = None
 		
 	def _correct_position(self):
 		self.position = np.maximum(self.position, self.lower)
@@ -249,8 +36,7 @@ class ParticleSwarm2:
 		self.velocity = np.maximum(self.velocity, self.V_min)
 		self.velocity = np.minimum(self.velocity, self.V_max)
 	
-	def evolve(self, gbest: np.ndarray):
-		self.gbest = gbest
+	def evolve(self):
 		self.velocity = self.velocity \
 			+ self.c1 * uniform(0, 1) * (self.pbest - self.position) \
 			+ self.c2 * uniform(0, 1) * (self.gbest - self.position)
@@ -265,7 +51,7 @@ if __name__ == '__main__':
 	p = Particle(dimension=2, upper_bound=np.array([1, 1]), lower_bound=np.array([0, 0]))
 	print(p.D, p.upper, p.lower)
 	"""
-	p = ParticleSwarm2(
+	p = ParticleSwarm(
 		dimension=2, population=100, upper_bound=np.array([1, 1]), lower_bound=np.array([0, 0]),
 		upper_velocity=np.array([-1, -1]), lower_velocity=np.array([1, 1]))
 	print(p.D, p.upper, p.lower, p.position, p.velocity)
